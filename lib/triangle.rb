166035c2ab2978002ddc9bc23ec86ccc22be37eb
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-22 01:48:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -44,12 +44,12 @@ class Triangle
   end
 
   def no_sides
-          @sides.each do |length|
-            if(length <= 0)
+          if@side1 = 0
+            @side2 = 0
+            @side3 = 0
           raise TriangleError
             end
           end
-        end
 
 end
 
